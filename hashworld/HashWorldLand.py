4f65469aeb2932c90b68f6eb55b83c09ca11e956
jackielg <jackie.lg@hotmail.com>
2018-05-24 11:18:45 +0800

    close console log

--- hashworld/HashWorldLand.py
+++ hashworld/HashWorldLand.py
@@ -12,10 +12,10 @@ from common import send_email
 logger = logging.getLogger("HashWorldLand.py")
 logger.setLevel(logging.INFO)  # Log等级总开关
 # 第二步，创建一个handler，用于写入日志文件
-fh = logging.FileHandler('./logs/HashWorldLand.log', mode='w')
+fh = logging.FileHandler('./logs/HashWorldLand.log', mode='w', encoding='UTF-8')
 fh.setLevel(logging.WARNING)  # 输出到file的log等级的开关
 ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)  # 输出到console的log等级的开关
+ch.setLevel(logging.ERROR)  # 输出到console的log等级的开关
 # 第三步，定义handler的输出格式
 formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
 fh.setFormatter(formatter)

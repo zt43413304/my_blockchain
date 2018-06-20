81a4a84a08639c27ae4720d9d473cf0822fcbe39
jackielg <jackie.lg@hotmail.com>
2018-06-20 21:18:54 +0800

    update

--- star163/my_star163_once.py
+++ star163/my_star163_once.py
@@ -448,7 +448,7 @@ def loop_star163():
         send_email.send_star163_HtmlEmail('newseeing@163.com', str(phone) + '的原力及黑钻', content)
         logger.warning('********** Sending Collect Email Complete!')
 
-    appium_calculate136()
+    # appium_calculate136()
     appium_calculate138()
 
 

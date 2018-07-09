27af7cc10104ed05d3d9340acdd1612cdbffdfc7
jackielg <jackie.lg@hotmail.com>
2018-07-09 18:11:06 +0800

    update

--- bixiang/bixiang_readnews.py
+++ bixiang/bixiang_readnews.py
@@ -80,9 +80,9 @@ def start_reading_news(filename):
             # stopevt = threading.Event()
             stopevt.set()
             logger.warning('~~~~~~~~~~ hour='+str(now.hour)+', minute='+str(now.minute))
-            break
+            sys.exit(0)
 
-    return
+
 
 
 

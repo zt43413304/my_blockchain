dbd615749ba9a0993b61bfdc14f0d573d70b8f47
Jackie.Liu <jackie.lg@hotmail.com>
2018-07-10 19:06:49 +0800

    update

--- star163/my_star163.py
+++ star163/my_star163.py
@@ -299,6 +299,22 @@ def get_allTotal(cookie):
 
 
 def loop_star163():
+    thread138 = AppiumStar163.AppiumStar('4.4.2', '127.0.0.1:62001', 4725, '13826090504')
+    # thread138.setName('13826090504')
+    # thread136.setDaemon(True)
+    thread138.start()
+    # thread136.join(10)
+    time.sleep(random.randint(5, 10))
+
+    # thread136.setDaemon(True)
+    thread136 = AppiumStar163.AppiumStar('4.4.4', '127.0.0.1:7555', 4723, '13601223469')
+    # thread136.setName('13601223469')
+    thread136.start()
+    # thread136.join(10)
+    time.sleep(random.randint(5, 10))
+
+
+
     curpath = os.getcwd()
     file = open(curpath + '/star163/data_star163.json', 'r', encoding='utf-8')
     data_dict = json.load(file)
@@ -332,21 +348,8 @@ def loop_star163():
         send_email.send_star163_HtmlEmail('newseeing@163.com', str(phone) + '的原力及黑钻', content)
         logger.warning('********** Sending Collect Email Complete!')
 
-    thread138 = AppiumStar163.AppiumStar('4.4.2', '127.0.0.1:62001', 4725, '13826090504')
-    # thread138.setName('13826090504')
-    # thread136.setDaemon(True)
-    thread138.start()
-    # thread136.join(10)
-    time.sleep(random.randint(5, 10))
-
-    # thread136.setDaemon(True)
-    thread136 = AppiumStar163.AppiumStar('4.4.4', '127.0.0.1:7555', 4723, '13601223469')
-    # thread136.setName('13601223469')
-    thread136.start()
-    # thread136.join(10)
-    time.sleep(random.randint(5, 10))
 
 
 # Start from here...
 # logger.warning('***** Start ...')
-# loop_star163()
+loop_star163()

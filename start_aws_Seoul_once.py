a601faf8cb1d5298cb8d1207649366bf0048fd57
jackielg <jackie.lg@hotmail.com>
2018-07-29 19:50:21 +0800

    update

---
+++ start_aws_Seoul_once.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import logging
+import os
+import time
+
+from bixiang import bixiang_readnews
+
+# 第一步，创建一个logger
+logger = logging.getLogger("start_aws_Seoul_once.py")
+logger.setLevel(logging.INFO)  # Log等级总开关
+# 第二步，创建一个handler，用于写入日志文件
+rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+# log_path = os.path.dirname(os.getcwd()) + '/logs/'
+log_path = os.getcwd() + '/logs/'
+log_name = log_path + 'start_aws_Seoul_once' + rq + '.log'
+logfile = log_name
+
+fh = logging.FileHandler(logfile, mode='w', encoding='UTF-8')
+fh.setLevel(logging.INFO)  # 输出到file的log等级的开关
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)  # 输出到console的log等级的开关
+
+# 第三步，定义handler的输出格式
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# 第四步，将logger添加到handler里面
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# start
+logger.warning('********** Start from start_aws_Seoul.py ...')
+bixiang_readnews.start_reading_news("data_bixiang_Seoul.json")

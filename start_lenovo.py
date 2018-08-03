56ff1f290747edfb7cda3c58155a7544d500289a
jackielg <jackie.lg@hotmail.com>
2018-08-03 18:06:29 +0800

    update

--- start_lenovo.py
+++ start_lenovo.py
@@ -45,7 +45,7 @@ scheduler = BlockingScheduler()
 #                   max_instances=4)
 scheduler.add_job(my_hashworld.loop_hashworld_no_land, "cron", hour="0,12", minute="5", args=["data_hashworld_Tokyo.json"], max_instances=4)
 scheduler.add_job(my_hashworld.loop_hashworld_no_land, "cron", hour="4,16", minute="5", args=["data_hashworld_Seoul.json"], max_instances=4)
-scheduler.add_job(my_blockcity.loop_blockcity, "cron", hour="3,9,15,21", minute="30", max_instances=4)
+scheduler.add_job(my_blockcity.loop_blockcity, "cron", hour="1,3,5,7,9,11,13,15,17,19,21,23", minute="30", max_instances=4)
 scheduler.add_job(my_star163.loop_star163, "cron", hour="3,9,15,21", minute="45", max_instances=4)
 
 

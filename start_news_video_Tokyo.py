f3d3b484f24be85b47ebae6210314c25da6f6e60
jackielg <jackie.lg@hotmail.com>
2018-09-27 18:26:41 +0800

    update

--- start_news_video_Tokyo.py
+++ start_news_video_Tokyo.py
@@ -10,13 +10,13 @@ from bixiang import bixiang_news_video
 
 # 第一步，创建一个logger
 
-logger = logging.getLogger("start_lenovo_news_video.py")
+logger = logging.getLogger("start_news_video_Seoul.py")
 logger.setLevel(logging.INFO)  # Log等级总开关
 # 第二步，创建一个handler，用于写入日志文件
 rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
 # log_path = os.path.dirname(os.getcwd()) + '/logs/'
 log_path = os.getcwd() + '/logs/'
-log_name = log_path + 'start_lenovo' + rq + '.log'
+log_name = log_path + 'start_news_video_Seoul' + rq + '.log'
 logfile = log_name
 
 fh = logging.FileHandler(logfile, mode='w', encoding='UTF-8')
@@ -35,12 +35,12 @@ logger.addHandler(fh)
 logger.addHandler(ch)
 
 # start
-logger.warning('********** Start from start_lenovo_news_video.py ...')
+logger.warning('********** Start from start_news_video_Seoul.py ...')
 scheduler = BlockingScheduler()
 
 # HP Sever
 scheduler.add_job(bixiang_news_video.start_news_video, "cron", hour="7,13,19", minute="30",
-                  args=["data_bixiang_Tokyo.json"], max_instances=4)
+                  args=["data_bixiang_Seoul.json"], max_instances=4)
 
 try:
     scheduler.start()

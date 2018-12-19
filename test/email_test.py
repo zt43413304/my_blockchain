9ac9475bfecf1c143e4d0abb79c21032a34beeb6
jackielg <jackie.lg@hotmail.com>
2018-12-19 14:33:59 +0800

    update

--- test/email_test.py
+++ test/email_test.py
@@ -1,3 +1,30 @@
-from common import send_email
+import smtplib
+from email.mime.text import MIMEText
 
-send_email.send_mail('newseeing@163.com', 'Subject', "Content")
+mailto_list = ['']  # 收件人(列表)
+mail_host = "smtp.163.com"  # 使用的邮箱的smtp服务器地址，这里是163的smtp地址
+mail_user = "newseeing"  # 用户名
+mail_pass = "Liuxb0504$"  # 密码
+mail_postfix = "163.com"  # 邮箱的后缀，网易就是163.com
+
+
+def send_mail(to_list, sub, content):
+    me = "newseeing@163.com"
+    msg = MIMEText(content, _subtype='plain')
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = "newseeing@163.com"
+    # msg['To'] = ";".join(to_list)  # 将收件人列表以‘；’分隔
+    try:
+        server = smtplib.SMTP()
+        server.connect(mail_host)  # 连接服务器
+        server.login(mail_user, mail_pass)  # 登录操作
+        server.sendmail(me, to_list, msg.as_string())
+        server.close()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+send_mail('newseeing@163.com', 'Subject1', "Content")

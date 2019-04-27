375cb6a4bb9642166892e1c0e19b1e050a346f67
jackie.liu <jackie.lg@hotmail.com>
2019-04-27 23:41:48 +0800

    update

--- epayapp/my_epay_once.py
+++ epayapp/my_epay_once.py
@@ -5,6 +5,6 @@ import sys
 
 from epayapp import my_epay
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 my_epay.loop_epay("my_epay_data.json")

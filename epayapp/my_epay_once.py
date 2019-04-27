05d428ab047d1d6b47143dcdff9cc27888a126de
jackie.liu <jackie.lg@hotmail.com>
2019-04-28 00:04:32 +0800

    update

--- epayapp/my_epay_once.py
+++ epayapp/my_epay_once.py
@@ -8,5 +8,6 @@ from epayapp import my_epay
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(os.path.split(rootPath)[0])
+sys.path.append('C:\\DevTools\\my_blockchain')
 
 my_epay.loop_epay("my_epay_data.json")
